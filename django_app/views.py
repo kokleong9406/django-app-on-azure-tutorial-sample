856c1fd81d8aaabde9f02dd87701a95cd3723e94
kokleong9406 <kokleong9406@gmail.com>
2022-12-22 12:00:39 +0800

    Test streamline cloud/local_docker deployment v2

--- django_app/views.py
+++ django_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from decouple import config
 
 # Create your views here.
 def main(request):
-    return HttpResponse("Hello")
+    return HttpResponse(f"Hello. {config("CLOUD_OR_LOCAL_DOCKER")}")
