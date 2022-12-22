a085416b1b564b7eff25a13182ea59c7799f4ca5
kokleong9406 <kokleong9406@gmail.com>
2022-12-22 11:33:33 +0800

    Test streamline cloud/local_docker deployment

--- manage.py
+++ manage.py
@@ -2,20 +2,37 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
+CLOUD_OR_LOCAL_DOCKER = config("CLOUD_OR_LOCAL_DOCKER")
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app_on_azure.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+
+    if CLOUD_OR_LOCAL_DOCKER == "LOCAL_DOCKER":
+        """Run administrative tasks."""
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app_on_azure.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)
+
+    elif CLOUD_OR_LOCAL_DOCKER == "CLOUD":
+        """Run administrative tasks."""
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app_on_azure.settings')
+        try:
+            from django.core.management import execute_from_command_line
+        except ImportError as exc:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ) from exc
+        execute_from_command_line(sys.argv)       
 
 
 if __name__ == '__main__':
